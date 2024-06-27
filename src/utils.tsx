af2c1927918839a3dba1d475bd30452e35898397
effectiefdesign <163732338+effectiefdesign@users.noreply.github.com>
2024-06-27 15:35:51 +0200

    Delete src directory

--- src/utils.tsx
+++
@@ -1,5 +0,0 @@
-export const getSecond = (time: number) => { return time % 60 }
-
-export const getMinute = (time: number) => { return Math.floor(time / 60) % 60 }
-
-export const getHour = (time: number) => { return Math.floor(Math.floor(time / 60) / 60 ) }
